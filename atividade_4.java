0e71faca7e3aa6d5977124c7e9246871d7c35268
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-09-25 15:48:27 +0100

    Add files via upload

---
+++ atividade_4.java
@@ -0,0 +1,23 @@
+public class atividade_4 {
+    public static void main(String[] args) {
+        char user_letter = "g";
+        switch (user_letter) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println(ch + " it is vowel letter");
+                break;
+            default:
+                System.out.println(ch + " it is constant letter");
+        }
+    } 
+}
+
+
