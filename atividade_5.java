0909ddee4448f49ac1a3a8ae00c2d2eb4caa445a
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-09-25 15:56:03 +0100

    Add files via upload

---
+++ atividade_5.java
@@ -0,0 +1,31 @@
+public class atividade_5 {
+    public static void main(String[] args) {
+        int user_number =  6;
+        switch (user_number) {
+            case 1:
+            System.out.println("today is monday");
+            break;
+            case 2:
+            System.out.println("today is Tuesday");
+            break;
+            case 3:
+            System.out.println("today is Wednesday ");
+            break;
+            case 4:
+            System.out.println("today is Thursday ");
+            break;
+            case 5:
+            System.out.println("today is Friday ");
+            break;
+            case 6:
+            System.out.println("today is Saturday ");
+            break;
+            case 7:
+            System.out.println("today is Sunday ");
+            break;    
+                
+        }
+
+        
+    }
+}
