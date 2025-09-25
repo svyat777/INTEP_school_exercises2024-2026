22b497d09fdc789859a79e5969f499c5192ed185
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-09-25 15:42:49 +0100

    Add files via upload

---
+++ atividade_3.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class atividade_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter numer: ");
+        double user_number = sc.nextDouble(); 
+        if(user_number < 10 && user_number == 10){
+            System.out.println("this book is cheap");
+        }else{
+            System.out.println("this book is expensive");
+        }
+
+        
+    }
+}
