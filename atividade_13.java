a6d5c2c08d9ecd97a9db9fa0359acc67bf2e66c1
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-10-10 09:16:32 +0100

    Update atividade_13.java

--- atividade_13.java
+++ atividade_13.java
@@ -1,14 +1,28 @@
 public abstract class atividade_13 {
     public static void main(String[] args) {
-       int water_gragus = 98;
+       int water_gragus = 88;
+
        if(water_gragus >= 100){
         System.out.println("water warming is:" + water_gragus);
         System.out.println("water is boiling");
-        
-       }else if(water_gragus < 100 && water_gragus > 70){
+       }
+
+       else if(water_gragus < 20 && water_gragus > 0){
+        System.out.println("water warming is:" + water_gragus);
+        System.out.println("water is cold");
+       }
+
+       else if(water_gragus < 70 && water_gragus > 20){
+        System.out.println("water warming is:" + water_gragus);
+        System.out.println("water is warm");
+       }
+
+       else if(water_gragus < 100 && water_gragus > 70){
         System.out.println("water warming is:" + water_gragus);
         System.out.println("water is almost boiling");
-       }else{
+       }
+
+       else{
         System.out.println("water warming is:" + water_gragus);
         System.out.println("water isnt boiling");
        }
