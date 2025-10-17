e2264580332b1bd9abbf109eebc9da161461e18c
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-10-17 09:20:35 +0100

    Add files via upload

---
+++ atividade_13dot1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class atividade_13dot1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int result;
+        System.out.print("enter first number: ");
+        int user_first_number = input.nextInt(); 
+        
+        System.out.print("enter operator(-,/,*,+): ");
+        char user_operator = input.next().charAt(0);
+ 
+        System.out.print("enter second number: ");
+        int user_second_number = input.nextInt(); 
+
+        if(user_operator == '+'){
+            result = user_first_number + user_second_number;
+            System.out.print("result is: " + result);
+        }else if(user_operator == '-'){
+            result = user_first_number - user_second_number;
+            System.out.print("result is: " + result);
+        }else if(user_operator == '/'){
+            result = user_first_number / user_second_number;
+            System.out.print("result is: " + result);
+        }else if(user_operator == '*'){
+            result = user_first_number * user_second_number;
+            System.out.print("result is: " + result);
+        }else{
+            System.out.print("error: invalid operator");
+        }
+
+    }
+}
+
+
