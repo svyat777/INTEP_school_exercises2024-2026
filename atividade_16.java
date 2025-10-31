046bae6795dc8d0ad79d2aa75b26c313193dd9f2
WasntAwesome <67332887+svyat777@users.noreply.github.com>
2025-10-31 09:10:52 +0000

    Add files via upload

---
+++ atividade_16.java
@@ -0,0 +1,11 @@
+public class atividade_16 {
+    public static void main(String[] args) {
+        String [] convidados = {"Ana","Olivia","Oscar","Maria","Francisco","Pedro","Miguel"};
+        System.out.println("Os convidados de festa sao:");
+        for(int i = 0; i < convidados.length; i++) {
+            System.out.println(convidados[i]);
+         }
+    }
+}
+
+
